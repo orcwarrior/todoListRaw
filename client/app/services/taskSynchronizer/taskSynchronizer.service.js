a094c003d679d5e2b010f83b1361983a61b9bd2a
orcwarrior <orcwarrior@o2.pl>
2017-01-25 06:33:35 +0100

    taskSynchronizer recovery

--- client/app/services/taskSynchronizer/taskSynchronizer.service.js
+++ client/app/services/taskSynchronizer/taskSynchronizer.service.js
@@ -1,26 +1,48 @@
 'use strict';
 
-function taskSynchronizerService(taskService) {
-  var lastEventWasError;
+function taskSynchronizerService(taskService, $q) {
+  var lastRequestWasError;
+  var synchronizingTasks;
+
   function init() {
     taskService.on('storageDB:error', function (err) {
-      lastEventWasError = true;
+      console.log("TaskSynchronizer: storageDB:error handler called!");
+      lastRequestWasError = true;
     });
-    taskService.on('storageDB:success', function (res) {
-      lastEventWasError = false;
-      var unsyncTasks = taskService.listUnsynchronized();
-      _.each(unsyncTasks, _tryToSynchronize)
+    taskService.on('storageDB:success', function (req) {
+      console.log("TaskSynchronizer: storageDB:success handler called!");
+      lastRequestWasError = false;
+      _tryToSynchronize(req);
     })
-    console.log("Task Synchronizer inited!");
-  } init();
+  }  init();
 
-  function _tryToSynchronize(unsyncTask) {
-    taskService[unsyncTask._unsyncAction](unsyncTask);
+  function _tryToSynchronize(req) {
+    if (lastRequestWasError) return console.info('Synchronization aborted: last request was error.')
+    if (synchronizingTasks) return console.info('Synchronization aborted: synchronization in process.')
+    console.log(req);
+
+    synchronizingTasks = true;
+    taskService.listUnsynchronized()
+      .then(function (unsynchronizedTasksList) {
+        var synchronizationActions = [];
+        _.each(unsynchronizedTasksList, function (unsyncTask) {
+          var action = unsyncTask._unsyncAction;
+          console.log("Synchronizing task: (%s/%s); action: %s", unsyncTask._id, unsyncTask._unsyncId, unsyncTask._unsyncAction)
+          synchronizationActions.push(taskService[action](unsyncTask));
+        })
+        $q.all(synchronizationActions).then(function () {
+          console.log("All synchronization tasks (%d) done!", synchronizationActions.length);
+          taskService.clearUnsynchronized()
+            .then(function () {
+              console.log("Synchronize tasks unset!");
+              synchronizingTasks = false; })
+        });
+      })
   }
 
   return {
     isDesynchronized: function () {
-      return lastEventWasError;
+      return lastRequestWasError;
     }
   }
 }
