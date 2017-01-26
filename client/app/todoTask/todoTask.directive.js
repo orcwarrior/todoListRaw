35ee6c159f132271200474908d44b59e35716f5a
orcwarrior <orcwarrior@o2.pl>
2017-01-26 02:57:03 +0100

    todoTask directive: taskService handling (actual tasks)

--- client/app/todoTask/todoTask.directive.js
+++ client/app/todoTask/todoTask.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('todoListApp')
-  .directive('todoTask', function () {
+  .directive('todoTask', function (taskService) {
     return {
       templateUrl: 'app/todoTask/todoTask.html',
       restrict: 'EA',
@@ -9,19 +9,28 @@ angular.module('todoListApp')
         task: '=task'
       },
       link: function (scope, element, attrs) {
+        scope.isUnsyncTask = function () {
+          return scope.task._unsyncAction;
+        }
         scope.toggleTaskDone = function () {
           scope.task.completed = !scope.task.completed;
+          updateTask();
         };
         scope.taskCallendarDay = function (date) {
           return moment(date).calendar();
         }
         scope.toggleNotification = function () {
           scope.task.notification = !scope.task.notification;
+          updateTask();
         }
         scope.removeTask = function () {
           if (window.confirm("Czy na pewno chcesz usunąć zadanie: "+scope.task.name+"?")) {
+            taskService.delete(scope.task);
             alert("Zadanie usunięte!");
           }
+        }
+        function updateTask() {
+          taskService.update(scope.task);
         }
       }
     };
