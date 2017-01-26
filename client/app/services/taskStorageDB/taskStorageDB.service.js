f591614efade1bf996b27f17bb1e6de840067058
orcwarrior <orcwarrior@o2.pl>
2017-01-26 02:55:39 +0100

    fixes to keep up with api

--- client/app/services/taskStorageDB/taskStorageDB.service.js
+++ client/app/services/taskStorageDB/taskStorageDB.service.js
@@ -5,13 +5,15 @@ function taskStorageDBService(api) {
     create: function (task) {
       return api.task.save(task).$promise; },
     read: function (taskId) {
-      return api.task.get({_id: taskId}).$promise; },
+      return api.task.get({taskId: task._id}).$promise; },
     update: function (task) {
       return api.task.update(task).$promise; },
     delete: function (task) {
-      return api.task.delete(task).$promise; },
+      return api.task.delete({taskId: task._id}).$promise; },
     list: function (userId, filters) {
-      return api.task.query(_.extend(filters, {_userId: userId})).$promise; }
+      var filters =_.extend({}, filters, {_userId: userId});
+      return api.task.query(filters).$promise;
+    }
   }
 }
 
