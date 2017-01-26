22360f54cb5b178333ccdfac62aef1bd0667dc85
orcwarrior <orcwarrior@o2.pl>
2017-01-26 22:22:38 +0100

    taskService and Synchronizer: Fixed offline working and request to relisting when some mutating call was done.

--- client/app/services/taskService/taskService.service.js
+++ client/app/services/taskService/taskService.service.js
@@ -2,35 +2,48 @@
 
 function taskService(taskStorageLocal, taskUnsyncStorageLocal, taskStorageDB, userService, promiseFromValue, $q) {
   var combinedUserTasks, dbUserTasks;
+
   function _getUserPromise(paramUserId) {
     var user = (paramUserId) ? {_id: paramUserId} : userService.getUser();
     return $q.when(user)
   }
+
   // taskStorageLocal.on('taskStorage:listUpdate', function (newList) {
   //   combinedUserTasks = newList; // List going to be overriden by further list() request
   // })
   function _pickTaskWithId(task1, task2) {
     return (task1._id) ? task1 : task2;
   }
+
+  function _genTaskConfig(method) {
+    return {refreshList: method !== 'read'};
+  }
+
   function serviceActionChainBuilder(method, task) {
-    console.trace('taskService(%s:%s)',method, task);
+    console.trace('taskService(%s:%s)', method, task);
+    var dbRequestSucceed;
     var promise = taskStorageDB[method](task)
       .then(function fulfilment(res) {
-        console.info('taskService(%s:%s):fulfilment',method, task);
+        console.info('taskService(%s:%s):fulfilment', method, task);
         var taskWithId = _pickTaskWithId(res, task);
         // TODO: Przenieść re-listowanie na po-synchronizacji!!!
         // if (method !== 'read')
         //   evtEmitter.emitEvent('storageDB:refreshList', taskWithId);
-        evtEmitter.emit('taskService:successRequest',
-          { refreshList: method !== 'read' });
+        dbRequestSucceed = true;
         return taskStorageLocal[method](taskWithId);
       }, function rejection(err) {
-        console.info('taskService(%s:%s):rejection',method, task)
+        console.info('taskService(%s:%s):rejection', method, task)
         // if (method !== 'read')
         //   evtEmitter.emitEvent('storageDB:refreshList', [task]);
-        evtEmitter.emit('taskService:errorRequest', err);
+        dbRequestSucceed = false;
         return taskUnsyncStorageLocal[method](task);
-      });
+      }).then(function (taskLS) {
+        if (dbRequestSucceed)
+          evtEmitter.emit('taskService:successRequest', _genTaskConfig(method));
+        else
+          evtEmitter.emit('taskService:errorRequest', _genTaskConfig(method));
+      })
+
     return promiseFromValue(promise);
   }
 
@@ -55,7 +68,7 @@ function taskService(taskStorageLocal, taskUnsyncStorageLocal, taskStorageDB, us
         promiseChain = taskStorageLocal.list(filters)
           .then(function (localStorageList) {
             localList = localStorageList;
-            console.log('taskService(list):local:%d',_.keys(localList).length);
+            console.log('taskService(list):local:%d', _.keys(localList).length);
             return _getUserPromise();
           })
           .then(function (user) {
@@ -63,11 +76,11 @@ function taskService(taskStorageLocal, taskUnsyncStorageLocal, taskStorageDB, us
             return taskStorageDB.list(curUser._id, filters);
           })
           .then(function fulfilment(dbList) {
-            console.log('taskService((list):db:fulfilment:%d',dbList.length);
+            console.log('taskService((list):db:fulfilment:%d', dbList.length);
             dbUserTasks = _.keyBy(dbList, '_id');
             combinedUserTasks = _.extend(localList, dbUserTasks); // database has higher prior
-            evtEmitter.emit('taskService:successRequest', combinedUserTasks);
-            evtEmitter.emit('taskService:tasksReloaded',combinedUserTasks);
+            evtEmitter.emit('taskService:successRequest');
+            evtEmitter.emit('taskService:tasksReloaded', combinedUserTasks);
             return combinedUserTasks;
           }, function rejection(err) {
             console.log('taskService((list):db:error');
--- client/app/services/taskSynchronizer/taskSynchronizer.service.js
+++ client/app/services/taskSynchronizer/taskSynchronizer.service.js
@@ -1,60 +1,93 @@
 'use strict';
 
-function taskSynchronizerService(taskService, $q) {
-  var lastRequestWasError = true; // So it will try to sync at begining.
+function taskSynchronizerService(taskService, api, $q) {
+  var previousRequestFailed;
   var synchronizingTasks;
 
   function init() {
-    taskService.on('taskService:errorRequest', function (err) {
+    taskService.on('taskService:errorRequest', function (config) {
       console.log("TaskSynchronizer: taskService:errorRequest handler called!");
-      lastRequestWasError = true;
+      previousRequestFailed = true;
+      _checkTasksListRefresh(config);
     });
     taskService.on('taskService:successRequest', function (config) {
       console.log("TaskSynchronizer: taskService:successRequest handler called!");
-      if (lastRequestWasError) tryToSynchronize(config);
-      else _checkTasksListRefresh(config);
-      lastRequestWasError = false;
-    })
-  }  init();
 
+      runSynchronization(config)
+        .then(function () {
+          if (!synchronizingTasks) {
+            _checkTasksListRefresh(config);
+          }
+        });
+      previousRequestFailed = false;
+    })
+    _forceSynchronization();
+  }
+
+  init();
+  function _forceSynchronization() {
+    previousRequestFailed = true;
+    runSynchronization({refreshList: true})
+     .then(function () {
+       if (!synchronizingTasks) {
+         _checkTasksListRefresh({refreshList: true});
+       }
+     });
+    previousRequestFailed = false;
+  }
   function _checkTasksListRefresh(config) {
-    console.log("TaskSynchronizer: Synchronization end!");
-    synchronizingTasks = false;
+    console.log("TaskSynchronizer: _checkTasksListRefresh!");
     if (config && config.refreshList) {
       console.log("TaskSynchronizer: Synchronization required list re-load!");
       taskService.list();
     }
   }
-  function tryToSynchronize(config) {
-    console.log("TaskSynchronizer: trySync by: %s", config);
-    // if (lastRequestWasError) return console.info('Synchronization aborted: last request was error.')
-    if (synchronizingTasks) return console.info('Synchronization aborted: synchronization in process.')
+
+  function runSynchronization(config) {
+    var deffered = $q.defer();
+    if (!previousRequestFailed) {
+      console.info('Synchronization aborted: last request was fine, so there\'s nothing to synchronize. .')
+      return $q.resolve();
+    }
+    if (synchronizingTasks) {
+      console.info('Synchronization aborted: synchronization in process.');
+      return $q.resolve();
+    }
 
     synchronizingTasks = true;
     taskService.listUnsynchronized()
       .then(function (unsynchronizedTasksList) {
-        if (_.isEmpty(unsynchronizedTasksList)) {
-          return console.log("TaskSynchronizer: No tasks to synchronize!");
-        }
+        // if (_.isEmpty(unsynchronizedTasksList)) {
+        //   synchronizingTasks = false;
+        //   return console.log("TaskSynchronizer: No tasks to synchronize!");
+        // }
         var synchronizationActions = [];
         _.each(unsynchronizedTasksList, function (unsyncTask) {
           var action = unsyncTask._unsyncAction;
-          console.log("TaskSynchronizer: Synchronizing task: (%s); action: %s", unsyncTask._id ||unsyncTask._unsyncId, unsyncTask._unsyncAction)
+          console.log("TaskSynchronizer: Synchronizing task: (%s); action: %s", unsyncTask._id || unsyncTask._unsyncId, unsyncTask._unsyncAction)
           synchronizationActions.push(taskService[action](unsyncTask));
         })
         $q.all(synchronizationActions).then(function () {
           console.log("TaskSynchronizer: All synchronization tasks (%d) done!", synchronizationActions.length);
           taskService.clearUnsynchronized()
             .then(function () {
-              _checkTasksListRefresh(config);
+              synchronizingTasks = false;
+              deffered.resolve();
             })
         });
       })
+    return deffered.promise;
   }
 
   return {
+    synchronize: function () {
+      return api.ping.get().$promise
+        .then(function fulfilment() {
+          _forceSynchronization();
+        });
+    },
     isDesynchronized: function () {
-      return lastRequestWasError;
+      return previousRequestFailed;
     }
   }
 }
