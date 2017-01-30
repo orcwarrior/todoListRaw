ab0d71f925abb59f4adafd15e6771ceb34d56052
orcwarrior <orcwarrior@o2.pl>
2017-01-30 04:36:40 +0100

    Main.ctrl: taskList refreshed after event emitted

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,17 +1,17 @@
 'use strict';
 
-function mainController(taskService, taskSynchronizer) {
+function mainController(taskService) {
   var vm = this;
-  vm.synchronizer = taskSynchronizer;
-  vm.currentTimeBetween = currentTimeBetween;
 
-  taskService.on('taskService:tasksReloaded', function (tasksList) {
-    // indexes after angular sort arent mutated so sort will happen here:
-    vm.userTasks = _.sortBy(tasksList, 'date');
-  })
-  function currentTimeBetween(task, nextTask) {
-    if (!nextTask) return false;
-    return moment().isBetween(task.date, nextTask.date, 'second');
+  getTasksList();
+  taskService.on('taskService:tasksChanged', function () {
+    getTasksList();
+  });
+
+  function getTasksList() {
+    taskService.list().then(function (tasksList) {
+      vm.userTasks = _.sortBy(tasksList, 'date');
+    });
   }
 }
 
