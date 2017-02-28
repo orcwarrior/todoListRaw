ddda694e134a309b1c9873299b5afd0a349283ee
orcwarrior <orcwarrior@o2.pl>
2017-02-28 05:10:28 +0100

    SW: Entry file fixed

--- client/sw.js
+++ client/sw.js
@@ -2,10 +2,10 @@ events = {};
 
 importScripts('./serviceWorker/generic.js');
 importScripts('./serviceWorker/cache.js');
-events.notifyEvt = importScripts('./serviceWorker/notifications.js');
+importScripts('./serviceWorker/notifications.js');
 
 self.addEventListener('install', events.generic.install);
-self.addEventListener('activate', events.generic.activate);
+self.addEventListener('activate', events.cache.activate);
 self.addEventListener('fetch', events.cache.fetch);
 
 self.addEventListener('push', events.notifyEvt.push);
