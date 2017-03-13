b973fe5e9b3d3f79f5e89c17ba5ba1ae07525c55
orcwarrior <orcwarrior@o2.pl>
2017-03-14 00:31:17 +0100

    Notifications: User.model updated.

--- server/api/User/User.model.js
+++ server/api/User/User.model.js
@@ -5,7 +5,13 @@ import mongoose from 'mongoose';
 var UserSchema = new mongoose.Schema({
   name: String,
   tasks: [{type: mongoose.Schema.Types.ObjectId, ref: 'Task'}],
-  lastSync: Date
+  lastSync: Date,
+  notificationSubscription: mongoose.Schema.Types.Mixed
 });
-
+UserSchema.set('toJSON', {
+  transform: function(doc, ret, options) {
+    delete ret.notificationSubscription;
+    return ret;
+  }
+});
 export default mongoose.model('User', UserSchema);
