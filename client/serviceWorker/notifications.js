e70dbd8e210ddcfb164375c0a65517d9d709bef2
orcwarrior <orcwarrior@o2.pl>
2017-03-14 00:34:24 +0100

    Notifications: Service Worker notifications handler (push/notificationclick)

--- client/serviceWorker/notifications.js
+++ client/serviceWorker/notifications.js
@@ -1,48 +1,38 @@
 /**
  * Created by Dariusz on 2017-02-18.
  */
-//noinspection JSAnnotator
-return (function () {
-  var notifyEvt = {};
 
-  notifyEvt.push = function (event) {
-    console.log('Received a push message', event);
+var notifyEvt = {};
 
-    const title = 'Yay a message.';
-    const body = 'We have received a push message.';
-    const icon = '/images/icon-192x192.png';
-    const tag = 'simple-push-demo-notification-tag';
+notifyEvt.push = function (event) {
+  console.log('Received a push message', event);
+  var data = event.data && event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/assets/images/icons/app-logo-notification-192.png',
+      tag: data.tag,
+      vibrate: data.vibrate && ['500', '300', '150', '50', '150', '50', '150']
+    })
+  );
+};
 
-    event.waitUntil(
-      self.registration.showNotification(title, {
-        body: body,
-        icon: icon,
-        tag: tag
-      })
-    );
-  };
+notifyEvt.notificationclick = function (event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn’t close the notification when you click on it
+  event.notification.close();
 
-  notifyEvt.notificationclick = function (event) {
-    console.log('On notification click: ', event.notification.tag);
-    // Android doesn’t close the notification when you click on it
-    // See: http://crbug.com/463146
-    event.notification.close();
-
-    // This looks to see if the current is already open and
-    // focuses if it is
-    event.waitUntil(clients.matchAll({
-      type: 'window'
-    }).then(function (clientList) {
-      for (var i = 0; i < clientList.length; i++) {
-        var client = clientList[i];
+  event.waitUntil(clients.matchAll({type: 'window'})
+    .then(function (clientList) {
+      for (let client of clientList)
         if (client.url === '/' && 'focus' in client) {
-          return client.focus();
+          client.focus();
+          break; // Odp. klient znaleziony, przerwanie pętli
         }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow('/');
-      }
-    }));
-  };
-  return notifyEvt;
-})();
+    }))
+  if (clients.openWindow) {
+    return clients.openWindow('/');
+  }
+}
+
+events.notifyEvt = notifyEvt;
