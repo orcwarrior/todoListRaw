ed495fbdb28ffe0e997f9916a8213c843d911146
orcwarrior <orcwarrior@o2.pl>
2017-03-13 22:59:07 +0100

    userService: Switched to $localForage

--- client/app/services/userService/userService.service.js
+++ client/app/services/userService/userService.service.js
@@ -1,12 +1,13 @@
 'use strict';
 
-function userServiceService(localForage, api, $q) {
+function userServiceService($localForage, api, $q) {
   var fetchedUser;
+
   function fetchDBUser(userLocal) {
     return api.user.get({userId: userLocal._id},
       function (dbUser) {
         fetchedUser = dbUser;
-        localForage.setItem('user', dbUser);
+        $localForage.setItem('user', dbUser);
         return fetchedUser;
       }).$promise;
   }
@@ -14,14 +15,14 @@ function userServiceService(localForage, api, $q) {
     return api.user.save({name: 'u' + moment().format()},
       function (createdUser) {
         fetchedUser = createdUser;
-        localForage.setItem('user', createdUser);
+        $localForage.setItem('user', createdUser);
         return createdUser;
       }).$promise;
   }
 
   function getUser() {
     if (fetchedUser) return fetchedUser;
-    return localForage.getItem('user').then(function (userLocal) {
+    return $localForage.getItem('user').then(function (userLocal) {
       if (userLocal) {
         fetchedUser = fetchDBUser(userLocal);
         return userLocal;//
